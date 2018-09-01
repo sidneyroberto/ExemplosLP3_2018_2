af68ea9a139e95693725231a7af1c866fe0c83ca
Sidney Sousa <sidneysousa@MacBook-Pro-de-Sidney.local>
2018-08-31 21:39:26 -0400

    apenas atualizando

--- workspace-angular/cep-client/public/js/controllers/localidade_controller.js
+++ workspace-angular/cep-client/public/js/controllers/localidade_controller.js
@@ -2,4 +2,14 @@ var app = angular.module('cep');
 app.controller('LocalidadeController', function ($scope, $http) {
 
     $scope.opcao = 'cep';
+    var enderecoServidor =
+        'https://arcane-dawn-67118.herokuapp.com/localidade/';
+
+    $scope.consultar = function () {
+        if ($scope.opcao === 'cep') {
+
+        } else {
+
+        }
+    };
 });
