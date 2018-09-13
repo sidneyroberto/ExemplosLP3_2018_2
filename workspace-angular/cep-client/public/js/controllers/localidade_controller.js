b1eeead1a3a7fa5df1ebcd98e38c776cbe6e52d8
Sidney Sousa <sidneysousa@MacBook-Pro-de-Sidney.local>
2018-09-12 22:48:41 -0400

    Adicionando o projeto blog-server

--- workspace-angular/cep-client/public/js/controllers/localidade_controller.js
+++ workspace-angular/cep-client/public/js/controllers/localidade_controller.js
@@ -2,14 +2,44 @@ var app = angular.module('cep');
 app.controller('LocalidadeController', function ($scope, $http) {
 
     $scope.opcao = 'cep';
+    $scope.localidades = [];
     var enderecoServidor =
         'https://arcane-dawn-67118.herokuapp.com/localidade/';
 
     $scope.consultar = function () {
-        if ($scope.opcao === 'cep') {
+        // Se a variável filtro tiver algum valor...
+        if ($scope.filtro) {
+            $scope.sucesso = false;
+            $scope.erro = false;
 
-        } else {
+            // Monta a url do serviço de consulta
+            var url = enderecoServidor;
+            if ($scope.opcao === 'cep') {
+                url += 'cep/';
+            } else {
+                url += 'logradouro/';
+            }
+            url += $scope.filtro;
 
+            // Faz a consulta ao serviço
+            $http
+                .get(url) // Retorna uma Promise
+                .then(
+                    // Em caso de sucesso...
+                    function (resposta) {
+                        $scope.localidades = resposta.data;
+                        $scope.sucesso = true;
+                    },
+                    // Em caso de erro...
+                    function (erro) {
+                        console.log(erro);
+                        $scope.erro = true;
+                    }
+                );
         }
+    };
+
+    $scope.limparResultados = function () {
+        $scope.localidades = [];
     };
 });
