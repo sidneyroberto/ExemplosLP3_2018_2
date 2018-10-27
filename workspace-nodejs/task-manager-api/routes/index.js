d155f90ad92d7d0efc81ceab3d7c4e239bca563c
Sidney Sousa <sidney.sousa@ifms.edu.br>
2018-10-26 22:32:06 -0400

    incrementando o projeto taskmanager-api

--- workspace-nodejs/task-manager-api/routes/index.js
+++ workspace-nodejs/task-manager-api/routes/index.js
@@ -7,4 +7,25 @@ router.get('/', (req, res) => {
   res.json('Oi!');
 });
 
+// Serviço para teste do deploy
+router.get('/tarefas', (req, res) => {
+  res.json([
+    {
+      descricao: 'Testar web service',
+      severidade: 'alta',
+      deadline: new Date()
+    },
+    {
+      descricao: 'Implementar interface com o usuário',
+      severidade: 'média',
+      deadline: new Date()
+    },
+    {
+      descricao: 'Fazer o deploy da aplicação',
+      severidade: 'baixa',
+      deadline: new Date()
+    }
+  ]);
+});
+
 export default router;
